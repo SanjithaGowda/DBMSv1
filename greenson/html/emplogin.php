6fa846a4dc20ba8b90f7e3cc25e8de6350821f2d
Sanjitha Gowda <37591117+SanjithaGowda@users.noreply.github.com>
2019-03-21 20:45:43 +0530

    Update emplogin.php

--- greenson/html/emplogin.php
+++ greenson/html/emplogin.php
@@ -1,33 +1,3 @@
-<?php
-session_start();
-include("config.php");
-$error="";
-$uname="";
-$psw = "";
-if(isset($_POST["reg_emp"])){
-    $uname = mysqli_real_escape_string($conn, $_POST['uname']);
-    $psw = mysqli_real_escape_string($conn, $_POST['psw']);
-    $emp_chk_query = "SELECT * FROM employees WHERE uname='$uname'";
-    $result = mysqli_query($conn,$emp_chk_query);
-    $emp = mysqli_fetch_assoc($result);
-    if($emp['uname']){
-        $pwd_orig = $emp['pwd'];
-        if(md5($psw) == $pwd_orig){
-            $_SESSION['emp']=$uname;
-            echo "header not set";
-           //header("Location: cuswelcome.php");
-        }
-        else{
-            $error = "Password entered is invalid.";
-        }
-    }
-    else{
-        $error = "Username does not exist.";
-    }
-   
-}
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -45,12 +15,11 @@ if(isset($_POST["reg_emp"])){
   
    <div id="mySidenav" class="sidenav">
        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+       <a href="home.html">Home</a>
        <a href="gallery.html">Veiw Gallery</a>
-       <a href="#">Place an order</a>
-       <a href="#">Track order</a>
        <a href="#contact">Contact us</a>
-       <a href="mainsignup.html">Create account</a> 
-       <a href="mainlogin.html">Login</a>
+       <a href="mainsignup.php">Create account</a> 
+       <a href="mainlogin.php">Login</a>
         
     </div>
 
@@ -60,6 +29,7 @@ if(isset($_POST["reg_emp"])){
     function openNav() {
         document.getElementById("mySidenav").style.width = "10%";
     }
+
     function closeNav() {
         document.getElementById("mySidenav").style.width = "0";
     }
@@ -70,7 +40,7 @@ if(isset($_POST["reg_emp"])){
 
 <h2 style ="text-align:  center">Employee Login </h2>
 
-<form action="emplogin.php" method="post">
+<form action="/emp_action_page.php">
   <div class="imgcontainer">
     <img src="../images/img_avatar2.png" alt="Avatar" class="avatar">
   </div>
@@ -82,14 +52,14 @@ if(isset($_POST["reg_emp"])){
     <label for="psw"><b>Password</b></label>
     <input type="password" placeholder="Enter Password" name="psw" required>
         
-    <button type="submit" name = "reg_emp" >Login</button>
+    <button type="submit">Login</button>
     <label>
       <input type="checkbox" checked="checked" name="remember"> Remember me
     </label>
-    <br>
-    <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
+  
+    <span class="psw">New user? <a href="#">Sign up</a></span>
+      <br>
       </div>
-      
 
  </form>
   
